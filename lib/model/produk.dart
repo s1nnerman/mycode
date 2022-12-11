b30e6496cfc21499a7b46b874c72f44a46aaf23a
s1nnerman <120293570+s1nnerman@users.noreply.github.com>
2022-12-11 11:51:35 +0700

    Add files via upload

---
+++ lib/model/produk.dart
@@ -0,0 +1,21 @@
+class Produk {
+  int? id;
+  String? kodeProduk;
+  String? namaProduk;
+  int? hargaProduk;
+
+  Produk(
+      {required this.id,
+      required this.kodeProduk,
+      required this.namaProduk,
+      required this.hargaProduk});
+
+  factory Produk.fromJson(Map<String, dynamic> obj) {
+    return Produk(
+      id: obj['id'],
+      kodeProduk: obj['kode_produk'],
+      namaProduk: obj['nama_produk'],
+      hargaProduk: obj['harga_produk'],
+    );
+  }
+}
---
+++ lib/ui/login_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:tokokita/ui/registrasi_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+
+  final _emailTextboxController = TextEditingController();
+  final _passwordTextboxController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                _emailTextField(),
+                _passwordTextField(),
+                _buttonLogin(),
+                const SizedBox(
+                  height: 30,
+                ),
+                _menuRegistrasi()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+// MEMBUAT TEXTBOX EMAIL
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Email"),
+      keyboardType: TextInputType.emailAddress,
+      controller: _emailTextboxController,
+      validator: (value) {
+        //VALIDASI HARUS DI ISI
+        if (value!.isEmpty) {
+          return 'Email harus diisi';
+        }
+        return null;
+      },
+    );
+  }
+
+//MEMBUAT TEXTBOX PASSWORD
+  Widget _passwordTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Password"),
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      controller: _passwordTextboxController,
+      validator: (value) {
+        // JIKA KARAKTER YANG DIMASUKKAN KURANG DARI 6 KARAKTER
+        if (value!.isEmpty) {
+          return "password harus diisi";
+        }
+        return null;
+      },
+    );
+  }
+
+// MEMBUAT TOMBOL LOGIN
+  Widget _buttonLogin() {
+    return ElevatedButton(
+        child: const Text("Login"),
+        onPressed: () {
+          var validate = _formKey.currentState!.validate();
+        });
+  }
+
+// MEMBUAT MENU UNTUK MEMBUKA HALAMAN REGISTRASI
+  Widget _menuRegistrasi() {
+    return Center(
+      child: InkWell(
+        child: const Text(
+          "Registrasi",
+          style: TextStyle(color: Colors.blue),
+        ),
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const RegistrasiPage()));
+        },
+      ),
+    );
+  }
+}
